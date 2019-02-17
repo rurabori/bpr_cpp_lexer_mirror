42d23c7f6bede8ec518c21ce90b42fc59b53b406
Boris Rúra <rura@avast.com>
2019-02-17 03:16:31 +0100

    updated main.

--- src/main.cpp
+++ src/main.cpp
@@ -7,52 +7,85 @@
 #include "ctle_concepts.h"
 #include "wise_enum.h"
 #include "states.h"
+#include "utils.h"
 
 using namespace ctle;
 
-WISE_ENUM(tokens, age, name, unknown)
+WISE_ENUM(tokens, age, name, unknown, eof, no_match)
 WISE_ENUM(states, (string, state_reserved), CLASS)
 
 
 class plus_operation {
 public:
 	tokens operator()(LexerInterface& b, std::string_view all, std::string_view first_num, std::string_view second_num) {
+		b.smd();
 		std::cout << all;
 		std::cout << " || = " << std::atoi(std::string(first_num.begin(), first_num.end()).c_str()) + std::atoi(std::string(second_num.begin(), second_num.end()).c_str()) << std::endl;
-		b.set_state(states::CLASS);
 		return tokens::age; 
 	}
 };
-
-class echo {
+/*
+class include {
 public:
-	template <typename... pack>
-	void operator()(LexerInterface& b, pack... all) {
-    	(std::cout << "ECHO : " << ... << all.to_view()) << '\n';
+	void operator()(LexerInterface& b, std::string_view all, std::string_view file_name) {
+		b.line_counter();
+		std::cout << file_name << std::endl;
+		b.add_file(file_name);
 	}
+};*/
+
+
+class lexer_extension
+{
+public:
+	void smd() { std::cout << "yay" << std::endl; }
 };
+
+class lineno_c
+{
+	size_t lineno{0};
+public:
+	void line_counter() { std::cout << ++lineno << std::endl; }
+};
+
+using x = class { int counter; };
+
+
+
+create_action(include, void, (LexerInterface& b, std::string_view all, std::string_view file_name){
+	b.line_counter();
+	std::cout << file_name << std::endl;
+	b.add_file(file_name);
+});
 
 int main(int argc, char const *argv[])
 {
-
-
-	lexer< tokens, 
+	using lexer_def = lexer< tokens, 
 				ctll::list<
 					lexer_rule<"int|double|rosticek|marienka|borisko|sasenka">,
-					lexer_rule<"[0-9]+(yrs)", echo, std::array{states::CLASS}>,
-					lexer_rule<"asd", echo, std::array{states::CLASS}>,
+					lexer_rule<"[0-9]+(yrs)", default_actions::echo>,
+					lexer_rule<"asd", default_actions::echo>,
+					lexer_rule<"#include \"(.*?)\"", include, std::array{ctle::all_states}>,
 					lexer_rule<"def">,
 					lexer_rule<"([0-9]+)\\s*\\+\\s*([0-9]+)", plus_operation>,
-					lexer_rule<"[ \t\r\n]+",nullptr_t, std::array{all_states}>
+					lexer_rule<"[ \t\r\n]+",nullptr_t, std::array{all_states}>,
+					lexer_rule<"test", default_actions::echo, std::array{states::string}>
 				>,
 				states,
-					std::array{state<states>{states::string, true}, state<states>{states::CLASS}}
-				> v22;
+					ctll::list<state<states::string, true>, state<states::CLASS>>,
+				derives_from<lexer_extension, lineno_c>
+				>;
+
+
+	lexer_def v22;
 
 	v22.add_file(argv[1]);
 
 
-	while (true) if (auto [matched, token] = v22.lex(); token.has_value() && token.value() == tokens::unknown) break;
+	while (true) if (auto [matched, token] = v22.lex(); token == tokens::no_match || token == tokens::eof) break;
+
+
+
 
 	return 0;
 }
