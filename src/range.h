788571c294b21b42f37efb343eff4efadf3e15e6
Boris Rúra <rura@avast.com>
2019-02-17 03:14:48 +0100

    Added some helpers, constexpr sorts, callable wrapper, etc.

---
+++ src/range.h
@@ -0,0 +1,25 @@
+#ifndef CTLE_RANGE
+#define CTLE_RANGE
+
+namespace ctle::utils {
+	/**
+	 * @brief	A small struct providing something of an "array_view".
+	 *
+	 * @tparam	T	Generic type parameter.
+	 */
+	template <typename T>
+	struct range {
+		T*      data{ nullptr };
+		size_t  size{ 0 };
+		
+        T* begin() noexcept { return data; }
+		
+        T* end() noexcept { return data+size; }
+
+		const T* begin() const noexcept { return data; }
+		
+        const T* end() const noexcept { return data+size; }
+	};
+}
+
+#endif // CTLE_RANGE
--- src/utils.h
+++ src/utils.h
@@ -4,7 +4,11 @@
 #include "ctll/fixed_string.hpp"
 #include "lexer_re.h"
 #include "states.h"
+
+
 #include <array>
+#include <optional>
+
 namespace ctle{
 	/**
 	 * @brief impl of list_to_type_t
@@ -49,5 +53,102 @@ namespace ctle{
 
 		return false;
 	}
+
+
+	namespace detail {	
+		// glorified std::apply which allows for varargs before|| THE VARARGS ARE APPLIED BEFORE TUPLE
+		template<typename Callable, typename... Args, typename Tuple, size_t... idx>
+		static constexpr CTRE_FORCE_INLINE auto apply_tuple(Callable&& f, Tuple&& tuple, std::index_sequence<idx...>, Args&&... args) {
+			return f(std::forward<Args>(args)..., tuple.template get<idx>()...);	
+		}
+		// yes this only swaps pair right now, not needed for anything else atm.
+		template<typename Ty>
+		static constexpr void swap(Ty&& a, Ty&& b) {
+			auto tmp = std::move(a);
+			a.first = b.first;
+			a.second = b.second;
+			b.first = tmp.first;
+			b.second = tmp.second;
+		}
+	}
+
+	// glorified std::apply which allows for varargs before || THE VARARGS ARE APPLIED BEFORE TUPLE
+	template<typename Callable, typename... Args, typename Tuple>
+	static constexpr CTRE_FORCE_INLINE auto apply_tuple(Callable&& f, Tuple&& tuple, Args&&... args) {
+		constexpr auto tuple_size = std::tuple_size_v<std::remove_reference_t<Tuple>>;
+		return detail::apply_tuple<Callable>(std::move(f), std::move(tuple), std::make_index_sequence<tuple_size>(), std::forward<Args>(args)...);	
+	}
+	
+	// just a bubble sort, not expecting long sequences of states.
+	template<typename Indexable, typename Comparator>
+	static constexpr auto sort(Indexable& to_sort, Comparator&& compare) {
+		auto i = 0;
+		auto j = 0;
+		for (i = 0; i < to_sort.size(); ++i) {
+			for (j = 0; j < to_sort.size() - i - 1; ++j) {
+				if (!compare(to_sort[j], to_sort[j+1]))
+					detail::swap(to_sort[j], to_sort[j+1]);
+			}
+		}
+	}
+
+	/**
+	 * @brief 	a wrapper for callable objects which distinguishes those which return and those which don't.
+	 * 			if a callable wishes to return conditionaly return std::optional<ReturnType> from that callable
+	 * 			and if it's empty, the caller won't return, if always returns, return ReturnType, if doesn't return,
+	 * 			specify return type as void.
+	 * 
+	 * @tparam Action a callable class.
+	 * @tparam ReturnType desired return_type if any.
+	 */
+	template<typename Action, typename ReturnType>
+    class callable {
+        using optional_return_t = std::optional<ReturnType>;
+    public:
+        template<typename... Args>
+        optional_return_t operator()(Args&&... args) {
+            using action_return_t = decltype(Action()(std::forward<Args>(args)...));
+			// distinguish between returning and non_returning ones.
+			if constexpr (!std::is_same_v<action_return_t, void>) {
+				// check if return type is correct.
+                static_assert(  std::is_same_v<action_return_t, ReturnType> || 
+                                std::is_same_v<action_return_t, optional_return_t>, 
+                                "Action has a wrong return type."
+                );
+                return optional_return_t{Action()(std::forward<Args>(args)...)};
+            } else {
+                Action()(std::forward<Args>(args)...);
+                return optional_return_t{};
+            }
+        }
+    };
+
+	/**
+	 * @brief a helper class, which is used to get a default callable when the specified one is not null.
+	 * 
+	 */
+	class get_default {
+		// implementation
+		template<typename Ty, typename Default>
+		static constexpr auto get_default_impl() {
+			if constexpr (std::is_same_v<Ty, std::nullptr_t>)
+				return Default();
+			else
+				return Ty();
+		}
+	public:
+		/**
+		 * @brief returns Ty if Ty is not std::nullptr_t else returns Default
+		 * 
+		 * @tparam Ty 
+		 * @tparam Default 
+		 */
+		template<typename Ty, typename Default>
+		using type_t = decltype(get_default_impl<Ty, Default>());
+	};
+
+	template<typename... Ty>
+	class derives_from : public Ty...
+	{};
 }
 #endif //CTLE_UTILS
