2b20d3b03bf26f54b61494926ad304e82083deb3
Boris Rúra <rura@avast.com>
2019-02-17 03:10:41 +0100

    Finished interface, added new template params. Updated documentation.

--- src/lexer.h
+++ src/lexer.h
@@ -7,41 +7,44 @@
 #include "utils.h"
 #include "ctle_concepts.h"
 #include "file_stack.h"
+#include "actions.h"
 
 #include "ctre.hpp"
 #include "ctll/list.hpp"
 #include "wise_enum.h"
 
-#include <iostream>
 #include <optional>
-#include <stack>
 #include <variant>
 
 namespace ctle {
-
 	/**
-	 * @brief holds all information needed for lexing.
+	 * @brief holds all information needed for lexing.	
 	 * 
 	 * @tparam ReturnType return type of actions in rules (fx. enum in which tokens are specified).
 	 * @tparam Rules a ctll::list of ctle::lexer_rules.
 	 * @tparam States an enum in which states are defined.
-	 * @tparam StateDefinitions an array of state<States> to forther clasify states as (non)exclusive.
+	 * @tparam StateDefinitions the definition of a state itself ((non)exclusive | eof action etc.).
+	 * @tparam DerivesFrom a derives_from specialization of all the base clases. This way you can add your own behavior to lexer (fx. count lines).
+	 * @tparam EofHandler a callable which gets called on eofs.
+	 * @tparam NoMatchHandler a handler if nothing matches.
+	 * @tparam FileType a class representing input file (needs to implement initialize() -> bool).
 	 */
 	template<	
 		typename ReturnType,
-		typename Rules = ctll::list<>, 
-		typename States = nullptr_t, 
-		std::array StateDefinitions = {}
+		typename Rules = ctll::list<>,
+		typename States = nullptr_t,
+		typename StateDefinitions = ctll::list<>,
+		typename DerivesFrom = derives_from<>,
+		typename EofHandler = default_actions::eof<ReturnType>,
+		typename NoMatchHandler = default_actions::no_match<ReturnType>,
+		typename FileType = basic_file<char>
 	>
-	class lexer
+	class lexer : public DerivesFrom
 	{
-	public:
-		using state_t = States;
-		using return_t = ReturnType;
-		using string_view_t = typename std::basic_string_view<char>;
-		using stack_t = ctle::basic_file_stack<ctle::basic_file<char>>;
-		using file_iterator_t = typename stack_t::iterator;
-
+		// a stack of files (used for includes etc.).
+		using stack_t = ctle::basic_file_stack<FileType>;
+		using file_iterator_t = typename FileType::iterator_t;
+		using file_range_t = typename stack_t::file_range;
 		/**
 		 * @brief Get all possible return types of rules
 		 * 
@@ -49,50 +52,41 @@ namespace ctle {
 		 * @return ctll::list<typename rule::template return_t<file_iterator_t>...> all possible return types.
 		 */
 		template<typename... rule>
-		static constexpr auto get_return_pack(ctll::list<rule...>) -> ctll::list<typename rule::template return_t<file_iterator_t>...>;
-
+		static constexpr auto get_return_pack(ctll::list<rule...>) -> ctll::list<typename rule::template return_t<file_iterator_t, file_iterator_t>...>;
+		// a small utility for the function above.
 		using return_pack_t = decltype(get_return_pack(std::declval<Rules>()));
 		/**
 		 * @brief a variant which holds all possible return values of lexer_rule::match
 		 */
-		using variant_return_t = ctle::unique_from_list_to_type_t<std::variant, return_pack_t>;
-		/**
-		 * @brief required signature of lexer_rule action.
-		 */
-		using action_signature_t = std::optional<ReturnType>(*)(lexer&, variant_return_t&&);
-	private:
-		/**
-		 * @brief return type of match function.
-		 */
-		using match_t = std::optional<ReturnType>;
+		using variant_return_t = ctle::unique_from_list_to_type_t<ctle::variant_match_wrapper, decltype(ctll::concat(ctll::list<empty_match_result<char>>(), return_pack_t()))>;
 		/**
 		 * @brief prototype of a match function as one is generated for each state.
 		 */
-		using match_signature_t = match_t (lexer::*)();
+		using match_signature_t = ReturnType (lexer::*)();
 		/**
 		 * @brief a pair of index / signature.
 		 */
 		using state_function_pair_t = std::pair<int, match_signature_t>;
 		/**
-		 * @brief an array holding a function pointer for each state (something like virtual table).
-		 */
-		std::array<state_function_pair_t, StateDefinitions.size() + 1> m_state_functions{state_function_pair_t{0, nullptr}};
-		/**
 		 * @brief current matching function (switched when state is switched).
 		 */
 		match_signature_t current_match_function{nullptr};	
 		// self explanatory.
-		std::string_view 		m_current_match;
 		stack_t 				m_stack{};
-		file_iterator_t 		m_begin{};
-		file_iterator_t 		m_end{};
-		variant_return_t 		m_return_value;
-		std::optional<return_t> m_return;
+		file_range_t			m_current_file{};
 
+		bool 					m_more{false};
 	public:
-		constexpr lexer() {
-			static_assert(wise_enum::is_wise_enum_v<States>, "Not a wise choice my friend, use WISE_ENUM");
-			fill_state_functions(std::make_index_sequence<StateDefinitions.size()>());	
+		using state_t = States;
+		using return_t = ReturnType;
+		using string_t = typename std::basic_string<typename FileType::char_t>;
+		/**
+		 * @brief the text matched by lexer on the last call of lex(). Available to actions as well.
+		 */
+		string_t text{};
+
+
+		lexer() {
 			set_state(state_initial);
 		}
 		/**
@@ -102,11 +96,12 @@ namespace ctle {
 		 * @return true on success.
 		 * @return false if no such state exists.
 		 */
-		constexpr bool set_state(int state) {
-			auto it = std::find_if(m_state_functions.cbegin(), m_state_functions.cend(), [state](const auto& r){ return r.first == state; });
-			
-			if (it == m_state_functions.cend()) return false;
+		bool set_state(int state) {
+			auto it = std::lower_bound(m_state_functions.cbegin(), m_state_functions.cend(), 
+											state_function_pair_t{state, nullptr}, [](const auto& a, const auto& b){ return a.first < b.first; });
 
+			if (it == m_state_functions.cend() || it->first != state) return false;
+			
 			current_match_function = it->second;
 			return true;
 		}
@@ -138,82 +133,142 @@ namespace ctle {
 		 */
 		template <typename... Args>
 		bool add_file(Args&&... c_args) {
+			// store the current location of lexed file.
+			m_stack.store(m_current_file.begin);
+			// create a new one.
 			auto retval = m_stack.push(std::forward<Args>(c_args)...);
 			if (!retval)
 				return false;
-			m_begin = m_stack.begin();
-			m_end = m_stack.end();
+			// reset the input iterators.
+			reset_iterators();
 			return true;
+		}
+		/**
+		 * @brief removes a file from lexer stack.
+		 * 
+		 * @return true if any files are left.
+		 * @return false otherwise.
+		 */
+		bool pop_file() noexcept {
+			if (m_stack.empty())
+				return false;
+
+			m_stack.pop();
+			reset_iterators();
+			return !m_stack.empty();
+		}
+		/**
+		 * @brief 	specifies to lexer, that the matched text should be kept and the next match should just append to it.
+		 * 			so if first rule matches "yes" and calls this, and the second rule matches " please", the text variable
+		 * 			for the next match will be "yes please".
+		 * 
+		 */
+		void more() noexcept {
+			m_more = true;
+		}
+		/**
+		 * @brief returns first n characters to the input. It's users responsibility to not return more than matched.
+		 * 
+		 * @param n how many characters should be returned, if 0 all matched text is returned.
+		 */
+		void less(size_t n = 0) {
+			n = (n) ? n : text.length();
+			std::advance(m_current_file.begin(), - n);
+			text.resize(text.length() - n);
 		}
 
 	private:
 		/**
-		 * @brief creates matching function for one state.
+		 * @brief updates the text variable, if more is set, appends to current text.
 		 * 
-		 * @tparam index of this state function.
+		 * @param view the view of the current match.
 		 */
-		template<auto index = 0> 
-		constexpr void fsf_impl() {
-			constexpr auto new_index = index + 1;
-			static_assert(static_cast<int>(StateDefinitions[index].value) >= state_reserved, "All states must begin at state_reserved.");
-			// create a filter for this state
-			using filter = state_filter<StateDefinitions[index].value>;
-			// take pointer to a method with a filtered set of rules.
-			auto function_ptr = &lexer::match_impl<typename filter::template filtered_t<Rules>>;
-			// add this matching function to array of all matching functions.
-			m_state_functions[new_index] = state_function_pair_t{static_cast<int>(StateDefinitions[index].value), function_ptr};
+		void update_text(auto view) {
+			if (m_more) {
+				text.append(view);
+				m_more = false;
+			} else {
+				text = view;
+			}
 		}
 		/**
-		 * @brief creates matching functions for all states.
-		 * 
-		 * @tparam idx index sequence used in fold.
+		 * @brief resets the current iterators to the new file. Needs to be called after each successful stack operation (push and pop).
 		 */
-		template <size_t... idx>
-		constexpr void fill_state_functions(std::index_sequence<idx...>) {
-			// this one is a bit special, as we do not check the value of state (starts at ...) also using different filter.
-			constexpr auto tmp = StateDefinitions; // gcc just does not like sending cnttp parameters around.
-			// create a filter for this state
-			using filter = initial_filter<tmp>;
+		void reset_iterators() {
+			m_current_file = m_stack.top();
+		}
+		/**
+		 * @brief create one pair of identifier/function
+		 * 
+		 * @tparam State the identifier of this state.
+		 * @tparam Filter the filter to be used on rules.
+		 * @tparam StateEofHandler the eof handler to be used for this state (if std::nullptr_t default used). 
+		 * @return assert_start wheter or not to assert the start of user defined states.
+		 */
+		template<int State, typename Filter, typename StateEofHandler, bool assert_start = true>
+		static constexpr auto make_state_function_pair() {
+			if constexpr (assert_start)
+				static_assert(State >= state_reserved, "All states must begin at state_reserved.");
 			// take pointer to a method with a filtered set of rules.
-			auto function_ptr = &lexer::match_impl<typename filter::template filtered_t<Rules>>;
+			constexpr auto function_ptr = &lexer::match_impl<get_default::type_t<StateEofHandler, EofHandler>, typename Filter::template filtered_t<Rules>>;
 			// add this matching function to array of all matching functions.
-			m_state_functions[0] = {state_initial, function_ptr};
-			// fill other state functions
-			(fsf_impl<idx>() , ...);
+			return state_function_pair_t{State, function_ptr};
+		}
+		/**
+		 * @brief creates matching functions for all states and sorts them by identifiers.
+		 * 
+		 */
+		template<typename... StateDefinition>
+		static constexpr auto make_state_functions(ctll::list<StateDefinition...>) {
+			std::array retval{
+					make_state_function_pair<state_initial, initial_filter<StateDefinitions>, std::nullptr_t, false>(),
+					make_state_function_pair<StateDefinition::identifier(), state_filter<StateDefinition::identifier()>, typename StateDefinition::eof_action_t>()...
+				};
+
+			sort(retval, [](const auto& a, const auto& b){ return a.first < b.first; });
+
+			return retval;
 		}
 		/**
 		 * @brief the function from which the matching functions are generated.
 		 * 
 		 * @tparam filtered_rules set of rules to use in this function.
-		 * @return match_t return of match.
+		 * @return return_t return of match.
 		 */
-		template <typename filtered_rules>
-		match_t CTRE_FORCE_INLINE match_impl() {
-			return match(filtered_rules());
+		template<typename eof_handler, typename filtered_rules>
+		return_t match_impl() {
+			return match<eof_handler>(filtered_rules());
 		}
 		/**
 		 * @brief real match function, uses fold statement to match all rules. Also moves input iterator by length of matched text.
 		 * 
 		 * @tparam rule_list rules to be used.
-		 * @return match_t value returned by action of a longest matched rule if it is returning, otherwise matches till such rule is encountered or EOF.
+		 * @return return_t value returned by action of a longest matched rule if it is returning, otherwise matches till such rule is encountered or EOF.
 		 */
-		template <typename... rule_list>
-		match_t CTRE_FORCE_INLINE match(ctll::list<rule_list...>) noexcept {			
-			while (m_begin != m_end) {
-				auto result = (lexer_result<variant_return_t,action_signature_t>{variant_return_t{}, nullptr} | ... | rule_list::template match<lexer>(m_begin, m_end));
-				if (!result.length())
-					break;
-				
-				std::advance(m_begin, result.length());
+		template<typename eof_handler, typename... rule_list>
+		return_t match(ctll::list<rule_list...>) noexcept {
+			std::optional<return_t> retval;
+			if (m_current_file.begin == m_current_file.end) {
+				retval = callable<eof_handler, return_t>()(*this);
+			} else {
+				auto result = (variant_return_t{} | ... | variant_return_t{rule_list::match(m_current_file.begin, m_current_file.end)});
+				if (!result.length()) 
+					retval = callable<NoMatchHandler, return_t>()(*this);
 
-				if (result.action) {
-					if (auto retval = result.action(*this, std::move(result.match)); retval)
-						return retval;
-				}
+				std::advance(m_current_file.begin, result.length());
+				update_text(result.to_view());
+
+				if (result.has_action()) 
+					retval = result.do_action(*this);
 			}
 
-			return ReturnType::unknown;
-		}	
+			return (retval) ? std::move(retval.value()) : (this->*current_match_function)();
+		}
+
+		/**
+		 * @brief an array holding a function pointer for each state (something like virtual table).
+		 */
+		static constexpr auto m_state_functions{make_state_functions(StateDefinitions())};
 	};
 
 }
