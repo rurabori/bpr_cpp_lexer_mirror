b574c7610d21d12b567319bf261b3adde207bee6
Boris Rúra <rura@avast.com>
2019-02-17 03:09:26 +0100

    Added documentation, switched the interface up a bit. Added default return value.

--- src/file_stack.h
+++ src/file_stack.h
@@ -1,32 +1,74 @@
 #ifndef CTLE_FILE_STACK
 #define CTLE_FILE_STACK
 #include "file.h"
-#include "stack_iterator.h"
 #include <vector>
+#include <iostream>
 
 namespace ctle {
 
-    template <typename file_type>
+    template <typename FileType>
     class basic_file_stack
     {
+        using element_t = std::pair<std::unique_ptr<FileType>, size_t>;
+        std::vector<element_t> m_files;
     public:
-        using char_type = typename file_type::char_type;
-        using iterator = stack_iterator<basic_file_stack<file_type>>;
-        
+        using file_t = FileType;
+        using char_t = typename file_t::char_t;
+        using iterator_t = typename file_t::iterator_t;
+
+        struct file_range {
+            iterator_t begin{};
+            iterator_t end{};
+        };
+
+        /**
+         * @brief tries to add a file to the stack, if fail, nothing changes stack_wise.
+         * 
+         * @tparam Args
+         * @param constructor_args forwarded to FileType creator.
+         * @return true if success false otherwise.
+         */
         template <typename... Args>
-        bool push(Args&&... constructor_args) { m_files.push_back(std::make_unique<file_type>(std::forward<Args>(constructor_args)...)); return true; }
-        iterator begin() noexcept { return iterator(this, m_files.size() - 1); }
-        iterator end() noexcept { return iterator(this, -1); }
+        bool push(Args&&... constructor_args) { 
+            auto new_file = FileType::create(std::forward<Args>(constructor_args)...);
 
-
-
-        void pop() { m_files.pop_back(); }
-        file_type* top() { return m_files.back().get(); }
-        file_type* operator[](size_t idx) const { 
-            return m_files[idx].get(); 
+            if (!new_file) return false;
+            m_files.emplace_back(std::move(new_file), 0);
+            
+            return true;
         }
-    private:
-        std::vector<std::unique_ptr<file_type>> m_files;
+        /**
+         * @brief stores the current position in the file on top of the stack.
+         * 
+         * @param current_position an iterator, that MUST belong to the file at the top of the stack.
+         */
+        void store(iterator_t current_position) {
+            if (!m_files.empty()) {
+                auto& current = m_files.back();
+                current.second = std::distance(current.first->begin(), current_position);            
+            }
+        }
+        /**
+         * @brief checks if stack is empty.
+         */
+        bool empty() const noexcept { return m_files.empty(); }
+        /**
+         * @brief removes a file from the stack.
+         */
+        void pop() { m_files.pop_back(); }
+        /**
+         * @brief returns the file on top of the stack, in the state that it was last stored (if offset was stored).
+         * 
+         * @return file_range, if stack is empty, an empty range is returned.
+         */
+        file_range top() { 
+            if (m_files.empty()) return file_range{};
+
+            auto& current = m_files.back();
+
+            return file_range{current.first->begin() + current.second, current.first->end()};
+        }
+
     };
 
 }
